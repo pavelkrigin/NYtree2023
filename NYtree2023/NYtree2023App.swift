f61c34bd191e330acc7402e6ef760d2c8e97db3f
Pavel Krigin <pavel.krigin.85@gmail.com>
2022-12-30 18:29:38 +0300

    Initial Commit

---
+++ NYtree2023/NYtree2023App.swift
@@ -0,0 +1,17 @@
+//
+//  NYtree2023App.swift
+//  NYtree2023
+//
+//  Created by Pavel Krigin on 30.12.22..
+//
+
+import SwiftUI
+
+@main
+struct NYtree2023App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
